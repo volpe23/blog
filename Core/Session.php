0efb445f0ec26d8afc8d90be498f7f210cddcd95
volpe23 <arozmianecs@gmail.com>
2025-04-04 16:25:36 +0300

    added session class

---
+++ Core/Session.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core;
+
+class Session
+{
+
+    public static function check($key): bool {
+        return (bool) $_SESSION[$key];
+    }
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key)
+    {
+        return $_SESSION[$key] ?? null;
+    }
+}
